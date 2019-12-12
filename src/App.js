7ddf124751d557169d0bc3f84d36d97dc0d12acf
Victor Radael <victorradael@hotmail.com>
2019-12-12 08:02:35 -0300

    reescrevendo_asyncstorage

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {AsyncStorage } from 'react-native';
+
+armazenadoInformacoes = async () => {
+  try {
+    await AsyncStorage.setItem('@MySuperStore:key', 'I like to save it.');
+  } catch (error) {
+    // Error saving data
+  }
+};
+
+buscandoInformacoes = async () => {
+  try {
+    const value = await AsyncStorage.getItem('TASKS');
+    if (value !== null) {
+      // We have data!!
+      console.log(value);
+    }
+  } catch (error) {
+    // Error retrieving data
+  }
+};
