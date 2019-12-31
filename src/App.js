30730d2de7a045de7d8f7abd4ce68ab39b2fcabc
Victor Radael <victorradael@hotmail.com>
2019-12-31 16:46:53 -0300

    implementandoBack

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
-import  Component  from 'react';
-import  AsyncStorage  from 'react-native';
+let amigoDoUsuario = [{
+  id: 19935629696, //(xx) x xxxx-xxxx
+  nome: 'Gabriel',
+  // idsAmigos: [3583540819, 3583426119] //Isso vai ter que ser buscado de algum lugar
+}]
 
-export default class App extends Component {
+let usuario = [{
+  id: 19935629695, //(xx) x xxxx-xxxx
+  nome: 'Victor',
+  amigos: [],
+  idsAmigos: []
+}]
 
-  // busca() {
-  //   const idsAmigos = usuario.getItem(amigos.id)
-  //   return idsAmigos
-  // }
-  salvandoInformacoes = async () => {
-    let amigoDoUsuario = [{
-      id: 19935629696, //(xx) x xxxx-xxxx
-      nome: 'Gabriel',
-    },{
-      idsAmigos: [3583540819, 3583426119 ], //Isso vai ter que ser buscado de algum lugar
-    }]
+console.log(amigoDoUsuario)
+console.log(usuario)
 
-    let usuario = {
-      id: 19935629695, //(xx) x xxxx-xxxx
-      nome: 'Victor',
-      amigos: [],
-      idsAmigos: [],
-    }
-    usuario.amigos.push(amigoDoUsuario)
-    usuario.idsAmigos.push(amigoDoUsuario.id)
-
-    try {
-      await AsyncStorage.setItem('usuarioInfo', JSON.stringify(eu)); // transformando o objeto em string
-    } catch (error) {
-      // Error saving data
-    }
-  };
-
-  buscandoInformacoes = async () => {
-    try {
-      const value = await AsyncStorage.getItem('usuarioInfo');
-      if (value !== null) {
-        // We have data!!
-        //console.log(JSON.parse(value).id);
-        console.log(JSON.parse(value).nome);
-        console.log(`IDs dos meus amigos ${JSON.parse(value).idsAmigos}`);
-        console.log(`IDs dos amigos dos meus amigos ${JSON.parse(value).amigos[0].idsAmigos}`); // transformar string em objeto novamente // percorrer os amigos
-      }
-      //console.log(value);
-    } catch (error) {
-      // Error retrieving data
-    }
-  };
-  render() {
-    this.salvandoInformacoes()
-    this.buscandoInformacoes()
-  }
-}
-
+const getAmigosDoAmigo = amigo => amigo.id
+let amigosDoAmigo = amigoDoUsuario.map(getAmigosDoAmigo)
+usuario.idsAmigos.concat(amigosDoAmigo) // CONCAT não funciona??
+console.log(amigosDoAmigo)
+console.log(usuario.idsAmigos)
