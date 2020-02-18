5564bd4b47e0bdbc22302ae8dddbd58b94216341
Victor Radael <victorradael@hotmail.com>
2020-02-18 18:36:43 -0300

    ATT REPO

--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://victor:victorvictor@clustertest-u58y1.mongodb.net/test?authSource=admin&replicaSet=ClusterTest-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',{
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-});
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
