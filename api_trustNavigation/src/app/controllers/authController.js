08b68384dcbafebe8c24b9245fb61068ce411731
victorradael <victorradael@hotmail.com>
2020-05-19 18:56:20 -0300

    :rocket: Initial Idea for Add Friend

--- api_trustNavigation/src/app/controllers/authController.js
+++ api_trustNavigation/src/app/controllers/authController.js
@@ -6,43 +6,72 @@ const { secret } = require("../../config/auth");
 const router = express.Router();
 
 function generateToken(params = {}) {
-    return jwt.sign(params, authConfig.secret, {
-        expiresIn: 86400,
-    });
-};
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 
-router.get('/', async (request, response) => { // Listar todos
-    try {
-        const user = await User.find().populate('user');
+router.get("/", async (request, response) => {
+  // Listar todos
+  try {
+    const user = await User.find();
+    console.log("ID 0 : Auth Controller");
+    console.log(user);
 
-        return response.send({ user })
-    } catch (error) {
-        return response.status(400).send({ error: 'Error loading Users' });
-    }
-
+    return response.send({ user });
+  } catch (error) {
+    return response.status(400).send({ error: "Error loading Users" });
+  }
 });
 
-
-
-router.post('/register', async (request, response) => {
-    const { foneNumber } = request.body;
-    console.log({foneNumber}) // Test ID 
-    try {
-        console.log(request.body)
-        if (await User.findOne({ foneNumber })) {
-            console.log(User) // Usuário registrado
-            return response.status(400).send({ error: `User already exists` })
-        }
-
-        const user = await User.create(request.body)
-
-        return response.send({
-            user,
-            token: generateToken({ id: user.id }),
-        })
-    } catch (error) {
-        return response.status(400).send({ error: `Registration Failed` })
+router.post("/register", async (request, response) => {
+  const { foneNumber } = request.body;
+  console.log({ foneNumber }); // Test ID
+  try {
+    console.log(request.body);
+    if (await User.findOne({ foneNumber })) {
+      console.log(User); // Usuário registrado
+      return response.status(400).send({ error: `User already exists` });
     }
+
+    const user = await User.create(request.body);
+
+    return response.send({
+      user,
+      token: generateToken({ id: user.id }),
+    });
+  } catch (error) {
+    return response.status(400).send({ error: `Registration Failed` });
+  }
+});
+
+router.post("/addfriend", async (request, response) => {
+  const { foneNumber } = request.body;
+  console.log(request.body);
+  console.log({ foneNumber }); // Test ID
+  try {
+    if (await User.findOne({ foneNumber })) {
+      const warning = await Warning.findOneByIdAndUpdate(
+        request.params.warningId,
+        {
+          title,
+          description,
+        },
+        { new: true }
+      );
+      return response.status(400).send({ error: `User already exists` });
+    }
+
+    const user = await User.create(request.body);
+    console.log(User); // Usuário registrado
+
+    return response.send({
+      user,
+      token: generateToken({ id: user.id }),
+    });
+  } catch (error) {
+    return response.status(400).send({ error: `Registration Failed` });
+  }
 });
 
 router.post("/authenticate", async (request, response) => {
