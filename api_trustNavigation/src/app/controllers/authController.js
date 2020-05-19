8235fd4c6abebb27ece2814c50f7f40ed8f4b3e0
victorradael <victorradael@hotmail.com>
2020-05-19 18:08:05 -0300

    :rocket: Routes Att, auth -> user and debug

--- api_trustNavigation/src/app/controllers/authController.js
+++ api_trustNavigation/src/app/controllers/authController.js
@@ -1,52 +1,55 @@
-const express = require('express')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-const authConfig = require('../../config/auth')
+const express = require("express");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { secret } = require("../../config/auth");
 
 const router = express.Router();
 
 function generateToken(params = {}) {
-    return jwt.sign(params, authConfig.secret, {
-        expiresIn: 86400,
-    });
-};
+  return jwt.sign(params, secret, {
+    expiresIn: 86400,
+  });
+}
 
-router.post('/register', async (request, response) => {
-    const { foneNumber } = request.body;
-    console.log({foneNumber}) // Test ID 
-    try {
-        console.log(request.body)
-        if (await User.findOne({ foneNumber })) {
-            console.log(User) // Usuário registrado
-            return response.status(400).send({ error: `User already exists` })
-        }
-
-        const user = await User.create(request.body)
-
-        return response.send({
-            user,
-            token: generateToken({ id: user.id }),
-        })
-    } catch (error) {
-        return response.status(400).send({ error: `Registration Failed` })
-    }
-});
-
-router.post('/authenticate', async (request, response) => {
-    const { foneNumber } = request.body;
-
-    const user = await User.findOne({ foneNumber });
-
-    if (!user) {
-        return response.status(400).send({ error: 'User not found' })
+router.post("/register", async (request, response) => {
+  const { foneNumber } = request.body;
+  console.log(` ID: 1 AuthController `); // Test ID
+  console.log({ foneNumber });
+  console.log(`ID: 2 AuthController `);
+  console.log(request.body);
+  try {
+    if (await User.findOne({ foneNumber })) {
+      return response.status(400).send({ error: `User already exists` });
     }
 
-    user.trustIds = undefined;
+    const user = await User.create(request.body);
+    console.log(`ID: 3 AuthController `);
+    console.log(user); // Usuário registrado
 
-    response.send({
-        user,
-        token: generateToken({ id: user.id }),
+    return response.send({
+      user,
+      token: generateToken({ id: user.id }),
     });
+  } catch (error) {
+    return response.status(400).send({ error: `Registration Failed` });
+  }
 });
 
-module.exports = app => app.use('/auth', router);
+router.post("/authenticate", async (request, response) => {
+  const { foneNumber } = request.body;
+
+  const user = await User.findOne({ foneNumber });
+
+  if (!user) {
+    return response.status(400).send({ error: "User not found" });
+  }
+
+  user.trustIds = undefined;
+
+  response.send({
+    user,
+    token: generateToken({ id: user.id }),
+  });
+});
+
+module.exports = (app) => app.use("/user", router);
