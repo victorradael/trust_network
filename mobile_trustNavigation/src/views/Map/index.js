b1ee167cf8a3f50668ba0274a09834c7bb7316ac
Victor Radael <victorradael@hotmail.com>
2020-05-28 20:14:02 -0300

    :rocket: MapView

--- mobile_trustNavigation/src/views/Map/index.js
+++ mobile_trustNavigation/src/views/Map/index.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 const Map = () => {
+  const [position, setPosition] = useState({
+    // latitude: 37.78825,
+    // longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      pos => {
+        setPosition({
+          ...position,
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+        });
+      },
+      error => {
+        console.log(error);
+        // Alert.alert('Houve um erro ao pegar a latitude e longitude.');
+      },
+    );
+  }, [position]);
+
   return (
-    <>
-      <Text>Hello</Text>
-      <Text>Map Screen</Text>
-      <Text>Here</Text>
-    </>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: position.latitude,
+          longitude: position.longitude,
+          latitudeDelta: position.latitudeDelta,
+          longitudeDelta: position.longitudeDelta,
+        }}
+      />
+    </View>
   );
 };
 
