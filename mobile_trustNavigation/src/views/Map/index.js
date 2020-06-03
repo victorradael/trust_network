96c62ca9425d3ce86372d1e53363e591ae62d165
Tailk Jardim <tkjardim@gmail.com>
2020-06-03 18:28:45 -0300

    :sparkles: added marker function (commited)

--- mobile_trustNavigation/src/views/Map/index.js
+++ mobile_trustNavigation/src/views/Map/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, {PROVIDER_GOOGLE, AnimatedRegion, Marker} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const styles = StyleSheet.create({
   container: {
@@ -21,6 +21,7 @@ const Map = () => {
     // longitude: -122.4324,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
+
   });
 
   useEffect(() => {
@@ -54,5 +55,67 @@ const Map = () => {
     </View>
   );
 };
+
+// const clickCoords = () => map.onclick.getCurrentPosition(
+//   clickPos => {
+//     setPosition({
+//       ...position,
+//       latitude: clickPos.coords.latitude,
+//       longitude: clickPos.coords.longitude,
+//     });
+ 
+
+
+// getInitialState() {
+//   return {
+//     coordinate: new AnimatedRegion({
+//       latitude: LATITUDE,
+//       longitude: LONGITUDE,
+//     }),
+//   };
+// }
+
+// componentWillReceiveProps(nextProps) {
+//   const duration = 500
+
+//   if (this.props.coordinate !== nextProps.coordinate) {
+//     if (Platform.OS === 'android') {
+//       if (this.marker) {
+//         this.marker._component.animateMarkerToCoordinate(
+//           nextProps.coordinate,
+//           duration
+//         );
+//       }
+//     } else {
+//       this.state.coordinate.timing({
+//         ...nextProps.coordinate,
+//         duration
+//       }).start();
+//     }
+//   }
+// }
+
+
+// ----------------xxx-----------------
+
+// render() {
+//   return (
+//     <MapView initialRegion={...}>
+//       <MapView.Marker.Animated
+//         ref={marker => { this.marker = marker }}
+//         coordinate={this.state.coordinate}
+//       />
+//     </MapView>
+//   );
+// }
+
+// const geoReferencePoint = () => {
+// <MapView initialRegion={}>
+// <Marker draggable
+//   coordinate={}
+//   onDragEnd={(e) => this.setState({ x: e.nativeEvent.coordinate })}
+// />
+// </MapView>
+// }  -- aqui seria um marcador que pode ser reposicionado no mapa (ideia inicial)
 
 export default Map;
