75723c17aa393c0e23af73f66b6b0c99b7c88596
Victor Radael <victorradael@hotmail.com>
2020-06-03 19:23:00 -0300

    :rocket: Api map problems

--- mobile_trustNavigation/src/views/Map/index.js
+++ mobile_trustNavigation/src/views/Map/index.js
@@ -3,55 +3,46 @@ import {StyleSheet, View} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
+import {Container} from './styles';
+
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 1,
   },
 });
 
 const Map = () => {
   const [position, setPosition] = useState({
-    // latitude: 37.78825,
-    // longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    latitude: '0',
+    longitude: '0',
   });
 
   useEffect(() => {
-    Geolocation.getCurrentPosition(
-      pos => {
-        setPosition({
-          ...position,
-          latitude: pos.coords.latitude,
-          longitude: pos.coords.longitude,
-        });
-      },
-      error => {
-        console.log(error);
-        // Alert.alert('Houve um erro ao pegar a latitude e longitude.');
-      },
-    );
-  }, [position]);
+    Geolocation.getCurrentPosition(pos => {
+      console.log(pos);
+      const coords = {
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+      };
+      console.log(coords);
+
+      setPosition(coords);
+    });
+  }, []);
 
   return (
-    <View style={styles.container}>
+    <Container>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        region={{
+        initialRegion={{
           latitude: position.latitude,
           longitude: position.longitude,
-          latitudeDelta: position.latitudeDelta,
-          longitudeDelta: position.longitudeDelta,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
       />
-    </View>
+    </Container>
   );
 };
 
