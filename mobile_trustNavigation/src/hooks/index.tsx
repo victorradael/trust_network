8dd1efe78535e99bed0dcf3159f96e2cef376013
Victor Radael <victor.radael@psvsistemas.com.br>
2020-11-10 22:44:07 -0300

    Remove old version

--- mobile_trustNavigation/src/hooks/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import {AuthProvider} from './auth';
-
-const AppProviders: React.FC = ({children}) => (
-  <AuthProvider>{children}</AuthProvider>
-);
-
-export default AppProviders;
